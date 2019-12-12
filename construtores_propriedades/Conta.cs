59fb4138ae30f5de70c8925b16aedc7a3adb75d0
joao Paulo <zheph.jp@gmail.com>
2019-12-12 16:29:35 -0300

    teste poo construtores e propriedades

---
+++ construtores_propriedades/Conta.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Globalization;
+
+namespace construtores_propriedades
+{
+	class Conta
+	{
+		public string Nome { get;private set; }
+		public int NumeroConta { get; set; }
+		public double Saldo { get;private set; }
+
+		public Conta() { }
+
+		public Conta(string nome, int numeroConta)
+		{
+			Nome = nome;
+			NumeroConta = numeroConta;
+		}
+
+		public Conta(string nome, int numeroConta, double saldo) : this(nome, numeroConta)
+		{
+			Saldo = saldo;
+		}
+
+		public void Deposito(double valor)
+		{
+			 Saldo += valor;
+
+		}
+
+		public void Saque (double valor)
+		{
+			 Saldo -= valor + 5;
+		}
+
+		public override string ToString()
+		{
+			return "Conta "
+				+ NumeroConta 
+				+ ", "
+				+ "Titular: "
+				+ Nome
+				+ ", "
+				+ "Saldo : $ " + Saldo.ToString("f2",CultureInfo.InvariantCulture);
+		}
+	}
+}
---
+++ construtores_propriedades/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace construtores_propriedades
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Conta cliente;
+			Console.Write("entre com o numero da conta ");
+			int numero = int.Parse(Console.ReadLine());
+			Console.Write("entre com o titular da conta ");
+			string nome = Console.ReadLine();
+			Console.Write("haverá depósito inicial (s/n)? ");
+			char resp =char.Parse((Console.ReadLine().ToUpper()));
+			if (resp =='S')
+			{
+				Console.WriteLine("entre com o valor do saldo inicial");
+				double saldoInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+				 cliente = new Conta(nome, numero, saldoInicial);
+			}
+			else
+			{
+				 cliente = new Conta(nome, numero);
+			}
+
+			Console.WriteLine("dados da conta:");
+			Console.WriteLine(cliente);
+			Console.WriteLine();
+
+			Console.WriteLine("entre um valor para deposito");
+			double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			cliente.Deposito(deposito);
+			Console.WriteLine();
+			Console.WriteLine("dados da conta atualizados: ");
+			Console.WriteLine();
+			Console.WriteLine(cliente);
+
+			Console.WriteLine("entre um valor para saque");
+			double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			cliente.Saque(saque);
+			Console.WriteLine();
+			Console.WriteLine("dados da conta atualizados: ");
+			Console.WriteLine();
+			Console.WriteLine(cliente);
+
+
+		}
+	}
+}
